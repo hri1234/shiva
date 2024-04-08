296a50ab8a431ef7b5f10c33f24754314e20baaa
hritik pandey <128885231+hri1234@users.noreply.github.com>
2024-04-08 18:44:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,8 @@ let allowedOrigins = [
   "https://pos-os-frontend-dnwvvdkxp-hritik-4642d12e.vercel.app",
   "http://localhost:3002",
    "http://localhost:3003",
-  "https://client-f11m42x5s-hritik-4642d12e.vercel.app"
+  "https://client-f11m42x5s-hritik-4642d12e.vercel.app",
+  "mail12.netlify.app"
  
 
 ];
